ad3f28367c6e20ed17a666ac9bf48d0c7adb8c42
Serge Labada <sergelabada@gmail.com>
2022-07-05 20:48:37 +0300

    Initial commit

--- app/src/main/java/com/example/sandbox/Variance.kt
+++ app/src/main/java/com/example/sandbox/Variance.kt
@@ -1,9 +1,12 @@
-class Barrel<T>(var item: T)
+class Barrel<out T>(val item: T)
 
 fun main(args: Array<String>) {
     var fedoraBarrel: Barrel<Fedora> =
         Barrel(Fedora("a generic looking fedora", 15))
     var lootBarrel: Barrel<Loot> = Barrel(Coin(15))
+//    var lootBarrel: Barrel<Loot> = Barrel(Fedora("a generic looking fedora 2", 25))
 
     lootBarrel = fedoraBarrel
+//    lootBarrel.item = Coin(15)
+    val myFedora: Fedora = lootBarrel.item
 }
