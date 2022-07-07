d0e6448a2d6717d9ebf52d48ea3ee7805dc5b9a0
Serge Labada <sergelabada@gmail.com>
2022-07-07 20:09:40 +0300

    Initial commit

--- app/src/main/java/com/example/sandbox/Extensions.kt
+++ app/src/main/java/com/example/sandbox/Extensions.kt
@@ -1,3 +1,6 @@
+val String.numVowels
+    get() = count { "aeiouy".contains(it) }
+
 fun String.addEnthusiasm(amount: Int = 1) = this + "!".repeat(amount)
 
 fun <T> T.easyPrint(): T {
@@ -9,4 +12,6 @@ fun main(args: Array<String>) {
     println("Madrigal has left the building".addEnthusiasm(5))
     "Madrigal has left the building".easyPrint().addEnthusiasm(5).easyPrint()
     42.easyPrint()
+
+    "How many vowels?".numVowels.easyPrint()
 }
