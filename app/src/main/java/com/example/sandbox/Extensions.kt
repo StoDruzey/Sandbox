0337d223cbbc47752a1376c97963245c9ec8a991
Serge Labada <sergelabada@gmail.com>
2022-07-07 19:57:13 +0300

    Initial commit

--- app/src/main/java/com/example/sandbox/Extensions.kt
+++ app/src/main/java/com/example/sandbox/Extensions.kt
@@ -1,10 +1,12 @@
 fun String.addEnthusiasm(amount: Int = 1) = this + "!".repeat(amount)
 
-fun Any.easyPrint() = println(this)
+fun <T> T.easyPrint(): T {
+    println(this)
+    return this
+}
 
 fun main(args: Array<String>) {
     println("Madrigal has left the building".addEnthusiasm(5))
-    "Madrigal has left the building".addEnthusiasm(5).easyPrint()
+    "Madrigal has left the building".easyPrint().addEnthusiasm(5).easyPrint()
     42.easyPrint()
-
 }
